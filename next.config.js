67a0e0315f151f57723dd9092427112fd998462a
papia <86232710+papiasharmin@users.noreply.github.com>
2023-04-11 17:00:22 +0900

    protect cridentials

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    mongodb_user: 'papiasharmin',
-    mongodb_pass: '7MfyyvryELf5HxiY',
-    mongodb_cluster: 'cluster0',
-    NEXTAUTH_URL:'http://localhost:3000',
-    NEXTAUTH_SECRET: 'papiasharim25121991',
+    mongodb_user: 'XXXXXXXXX',
+    mongodb_pass: 'XXXXXXXXXXX',
+    mongodb_cluster: 'XXXXXXXX',
+    NEXTAUTH_URL:'XXXXXXXX',
+    NEXTAUTH_SECRET: 'XXXXXXXXXX',
 
   },
   images: {
@@ -16,16 +16,6 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-// const withVideos = require('next-videos')
 
-// module.exports = withVideos({
-//   basePath: '/v2',
-
-//   webpack(config, options) {
-//     return config
-//   }
-// })
-
-//mongodb+srv://papiasharmin:7MfyyvryELf5HxiY@cluster0.lhg764a.mongodb.net/?retryWrites=true&w=majority
 
 
